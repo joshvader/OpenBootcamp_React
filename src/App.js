c6a6bb7a8fa9ac150110639ac8d3a6754bdfe000
proyect_icaro <josuevalenzuela87@gmail.com>
2022-08-15 11:46:03 -0400

    Ejercicio Sesión 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ContactListComponent from './components/container/contact_list';
+import Clock from './components/Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ContactListComponent/>
+        {/*<ContactListComponent/>*/}
+        <Clock/>
       </header>
     </div>
   );
